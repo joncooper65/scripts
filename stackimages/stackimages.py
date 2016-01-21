87415567322bc1486ad89cf6917b8dc4fbdfb55f
joncooper65 <jcoop@ceh.ac.uk>
2016-01-21 20:45:20 +0000

    started the meteor star trails - not yet working

--- stackimages/stackimages.py
+++ stackimages/stackimages.py
@@ -15,7 +15,6 @@ def maxStack():
 	finalimage.save(filename,"JPEG")
 	return filename
 
-
 #Get the max across all images in the folder and multiply to enhance
 def stackMaxAndStretch():
 	filename = maxStack()
@@ -65,5 +64,41 @@ def maxStackCollectAll():
 		filename = "stackmax" + str(i).zfill(3) +".jpg"
 		finalimage.save(filename,"JPEG")
 
-maxStackCollectAll()
+#Use this for pulling out the max value of a pixel across images, and save an image with each iteration
+#This can be used to animate a star trail
+#Adapted to create 'slow meteor' effect
+#Example of images to video: ffmpeg -i stackmax%03d.jpg -c:v libx264 startrail.mp4
+def meteorStack():
+	files = glob.glob("./IMG*.*.JPG")
+	files.sort()
+	finalimage = Image.open(files[0])
+	initialFrames = 20
+	for i in range(0,initialFrames):
+		print str(i) + " of " + str(initialFrames)
+		currentimage=Image.open("./"+files[i])
+		finalimage=ImageChops.lighter(finalimage, currentimage)
+		filename = "stackmax" + str(i).zfill(3) +".jpg"
+		finalimage.save(filename,"JPEG")
+	start = initialFrames
+	stop = initialFrames * 2
+	for i in range(initialFrames, len(files)):
+		finalimage = Image.open(files[i])
+		print 'start stop: ' + str(start) + ' ' + str(stop)
+		for j in range(start, stop):
+			print str(j)
+#			currentimage=Image.open("./"+files[start-j])
+
+			currentimage = np.asarray(Image.open("./"+files[start-j]))
+			currentimage = currentimage.astype('uint32')
+#			currentimage = currentimage/j
+			currentimage = Image.fromarray(currentimage.astype('uint8'))
+
+			finalimage=ImageChops.lighter(finalimage, currentimage)
+		filename = "stackmax" + str(i).zfill(3) +".jpg"
+		finalimage.save(filename,"JPEG")
+		start += 1
+		stop = start + initialFrames
+		print 'havent quite got start stop and the iteration direction correct yet!'
+
+meteorStack()
 
